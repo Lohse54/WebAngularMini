eb6347ecccebd8a511c3f748a6d6e4506ef37bc0
Andreas Lohse <lohsea@hotmail.com>
2018-05-20 15:33:08 +0200

    Added comments

--- WebMini/AddGame.js
+++ WebMini/AddGame.js
@@ -1,19 +1,22 @@
+
 var gameApp = angular.module('gameApp', []);
 gameApp.controller('gameCtrl', function($scope){
     $scope.games = [];
 
+    //Array that contains all the games shown on the page.
     $scope.gameList = [
-        "God of War",
-        "Battlefield 4",
-        "Infamous: Second Son",
-        "Mass Effect 3"
-        ];
+        { game: "God of War", img: "GOW.jpg"},
+        { game: "Battlefield 4", img: "BFF4.jpg"},
+        { game: "Infamous: Second Son", img: "ISS.jpg"},
+        { game: "Mass Effect 3", img: "ME3.png"}
+    ];
 
     $scope.addGame = function () {
         $scope.games.push($scope.gameName);
     };
 });
 
+// Function for retreiving information for showing images and text
 function showImg(imgs) {
     var expandImg = document.getElementById("expandedImg");
     var imgText = document.getElementById("imgtext");
