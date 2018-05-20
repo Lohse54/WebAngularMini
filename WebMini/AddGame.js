fd61a0f1c1e63a644debe86318a9ced86f45f2bb
Andreas Lohse <lohsea@hotmail.com>
2018-05-20 12:26:25 +0200

    Added images and selection of images

--- WebMini/AddGame.js
+++ WebMini/AddGame.js
@@ -2,7 +2,22 @@ var gameApp = angular.module('gameApp', []);
 gameApp.controller('gameCtrl', function($scope){
     $scope.games = [];
 
+    $scope.gameList = [
+        "God of War",
+        "Battlefield 4",
+        "Infamous: Second Son",
+        "Mass Effect 3"
+        ];
+
     $scope.addGame = function () {
         $scope.games.push($scope.gameName);
     };
 });
+
+function showImg(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+}
